2d6dfb15862c27575b70d9734ae89851fba2c8f9
jjungyoon06 <jjungyoon06@gmail.com>
2022-05-20 18:18:34 +0900

    자바스크립트 소스 day10

---
+++ day10/js/calc.js
@@ -0,0 +1,12 @@
+//덧셈 프로그램
+let n1=parseInt(prompt("첫 번째 수 입력:"));
+let n2=parseInt(prompt("두 번째 수 입력:"));
+let sum_v=0;
+
+//연산
+//입력받은 문자를 숫자롭 변환함
+
+sum_v=n1+n2;
+
+//출력
+document.write("두 수의 합: "+sum_v);
