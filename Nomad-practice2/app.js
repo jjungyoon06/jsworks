c72bef87f67817e92aa284d75bf4a6b3cb17db0e
jjungyoon06 <jjungyoon06@gmail.com>
2022-08-14 22:55:39 +0900

    js

--- Nomad-practice2/app.js
+++ Nomad-practice2/app.js
@@ -2,6 +2,15 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick(){
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    console.log(username);
+
+    /*
+    빈 칸이면 이름 쓰라고 공지하고 15자 이하로 작성하게 하기
+    if(username === ""){
+        alert("Please write your name");
+    } else if(username.length > 15){
+        alert("Your name is too long");
+    }*/
 }
 loginButton.addEventListener("click",onLoginBtnClick);
